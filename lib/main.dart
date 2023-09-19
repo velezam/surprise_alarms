6ae60b63c50b144ff22d89096357e92ad8bfe93d
Amadeo Velez <amadeo.velez@gmail.com>
2023-09-18 21:28:36 -0400

    added Firebase core plugins and imports

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
+// Import firebase core plugin and configuration file
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
@@ -12,6 +16,11 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
